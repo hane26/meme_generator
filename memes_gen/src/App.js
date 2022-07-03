92c6634ff93e7bd77a6c970fe8d8b7b6b1140c85
hane <adrplusrayane@hotmail.com>
2022-07-03 12:46:59 +0100

    started the web app

---
+++ memes_gen/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Header from './Components/Header';
+function App() {
+  return (
+    <div className="App">
+    {/* <h1>memes Generator! </h1> */}
+    <Header />
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ memes_gen/src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import m from './images/meme.png';
+//import './header.css'
+
+function Header() {
+  return (
+    <Header className="header">
+        <img src={m} className="meme_pic" alt="meme" />
+        <h2 className="header--h2"> Header componenet </h2>
+        <h4 className="header--h4"> React - Course </h4>
+    </Header>
+
+  )
+}
+
+export default Header;
